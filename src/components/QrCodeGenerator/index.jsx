da391a26010842490e2d87fd38c4e7f9d3ed8504
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-06-11 16:57:18 +0530

    qrcode

--- src/components/QrCodeGenerator/index.jsx
+++ src/components/QrCodeGenerator/index.jsx
@@ -1,5 +1,51 @@
+import QRCode from "react-qr-code";
+import "./style.css";
+import { useEffect, useState } from "react";
 const QrGenerator = () => {
-  return <div>index</div>;
+  const [value, setValue] = useState("");
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
+  const qrGeneratorHandler = () => {
+    if (!input) {
+      setError("please enter value for QR");
+    } else {
+      setValue(input);
+    }
+  };
+  useEffect(() => {
+    if (input) {
+      setError("");
+    }
+  }, [input]);
+  return (
+    <div className="qr-container">
+      <input
+        className="qr-input"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter Text for Generate QrCode"
+        required
+      />
+      <p className="error-style">{error}</p>
+      <button onClick={qrGeneratorHandler} className="generate-qr-btn">
+        Generate QrCode
+      </button>
+      {value && (
+        <QRCode
+          size={100}
+          style={{
+            height: "400px",
+            maxWidth: "100%",
+            width: "100%",
+            marginBlock: "20px",
+          }}
+          value={value}
+          viewBox={`0 0 256 256`}
+        />
+      )}
+    </div>
+  );
 };
 
 export default QrGenerator;
